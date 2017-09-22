83eb8ff2d7005e5e2c649b68a56df5aa19b6ec53
k0r0tk0ff <korotkov34reg@gmail.com>
2017-09-22 21:35:38 +0300

    initial

---
+++ src/main/java/ru/k0r0tk0ff/rest/Greeting.java
@@ -0,0 +1,23 @@
+package ru.k0r0tk0ff.rest;
+
+/**
+ * Created by k0r0tk0ff on 9/22/2017.
+ */
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
